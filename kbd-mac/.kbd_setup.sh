5f6410069c8aa1d6d0ee267a62abccf5183cd4c9
James Hughes <james@iauns.com>
2014-11-16 23:17:09 -0800

    Add correct key configuration.

--- kbd-mac/.kbd_setup.sh
+++ kbd-mac/.kbd_setup.sh
@@ -5,38 +5,26 @@
 # See: http://cs.gmu.edu/~sean/stuff/n800/keyboard/old.html
 # 
 # http://www.in-ulm.de/~mascheck/X11/xmodmap.html
+# See: https://wiki.archlinux.org/index.php/xmodmap
 
 # Clear the logical modifiers
-xmodmap -e "clear Shift"
-xmodmap -e "clear Lock"
-xmodmap -e "clear Control"
-xmodmap -e "clear Mod1"
-xmodmap -e "clear Mod2"
-xmodmap -e "clear Mod3"
-xmodmap -e "clear Mod4"
-xmodmap -e "clear Mod5"
+#xmodmap -e "clear Shift"
+#xmodmap -e "clear Lock"
+#xmodmap -e "clear Control"
+#xmodmap -e "clear Mod1"
+#xmodmap -e "clear Mod2"
+#xmodmap -e "clear Mod3"
+#xmodmap -e "clear Mod4"
+#xmodmap -e "clear Mod5"
 
-xmodmap -e "add shift = Shift_L Shift_R"
-xmodmap -e "add control = Control_L Super_R"
-xmodmap -e "add mod1 = Meta_L Meta_R"
-
-# Clear caps lock then re-assign it to escape.
-echo "caps lock to escape"
-xmodmap -e "keycode 66 = Escape"
-
-# # Change right command into control.
-echo "Right command into control"
-#xmodmap -e "add control = Super_R"
-xmodmap -e "keycode 0x86 = Control_R"
+#xmodmap -e "add shift = Shift_L Shift_R"
+#xmodmap -e "add control = Control_L Super_R"
+#xmodmap -e "add mod1 = Meta_L Meta_R"
 
 # Now remap Left command to Mode_switch.
 # We use Mode_switch as a general key to modify basic interaction.
-echo "Left command to mode switch"
+#echo "Left command to mode switch"
 xmodmap -e "keycode 0x85 = Mode_switch"
-
-# Remap Right command to Control
-echo "Right command to control"
-#xmodmap -e "keycode 0x86 = Control_R"
 
 # All of the following can be done inside of i3.
 # Make Mode_switch-T issue an 'enter'
